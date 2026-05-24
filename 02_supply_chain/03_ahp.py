1273ca282c04a3390567e93c5751431de4fa94de
Jefeson Alves <jefesonbezerra@gmail.com>
2026-05-24 16:43:57 -0300

    Iniciado o método AHP

---
+++ 02_supply_chain/03_ahp.py
@@ -0,0 +1,6 @@
+"""
+Mestrado Profissional em Administração - UNP
+Disciplina: Gestão da Cadeia de Suprimentos (Gestão de Operações)
+Exercício B - Método AHP
+Professora: Luciana Gondim de A. Guimarães
+"""
