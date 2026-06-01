55bcd1f5466c9f54788dfd3433a1a9fb740a0dc3
Jefeson Alves <jefesonbezerra@gmail.com>
2026-05-31 22:52:12 -0300

    Menu de escolha de soluções

--- 02_supply_chain/03_ahp.py
+++ 02_supply_chain/03_ahp.py
@@ -4,3 +4,28 @@ Disciplina: Gestão da Cadeia de Suprimentos (Gestão de Operações)
 Exercício B - Método AHP
 Professora: Luciana Gondim de A. Guimarães
 """
+print('\033c', end='')
+import numpy as np
+
+def menu_solucoes():
+    solucoes = []
+    while True:
+        print("--- Menu de Soluções ---")
+        print("1 - Inserir Solução")
+        print("2 - Sair")
+
+        opcao = input("Escolha uma opção: ").strip()
+
+        if opcao == "1":
+            criterio = input("\nDigite o nome da solução: ").strip()
+            solucoes.append(criterio)            
+            
+        elif opcao == "2":
+            print("Saindo...")
+            break
+        
+        else:
+            print("Opção inválida!")
+    print(f"Soluçôes: {solucoes}")
+
+menu_solucoes()
